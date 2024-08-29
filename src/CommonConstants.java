d205d85b2edf4a872696de11727379c0db9f39d6
Wesley Santos <84673115+iW3ll@users.noreply.github.com>
2024-08-29 15:12:04 -0300

    update dimensão da janela em geral

--- src/CommonConstants.java
+++ src/CommonConstants.java
@@ -2,7 +2,7 @@ import java.awt.*;
 
 public class CommonConstants {
     // frame config
-    public static final Dimension GUI_SIZE = new Dimension(540, 760);
+    public static final Dimension GUI_SIZE = new Dimension(540, 700);
 
     // banner config
     public static final Dimension BANNER_SIZE = new Dimension(GUI_SIZE.width, 50);
